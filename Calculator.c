ce95db859ca8697a39b009eda1327600e830ff18
Nitin1112 <nitinboopathy11@gmail.com>
2025-10-03 01:15:25 +0530

    Completed Calculator operation with DMAS

--- Calculator.c
+++ Calculator.c
@@ -2,83 +2,126 @@
 #include<stdlib.h>
 #include<string.h>
 #include<ctype.h>
+
 int main(){
     char inputStr[1000];
     char calStr[1000];
-    int j=0;
-    float num1;
-    float num2;
-    
-    fgets(inputStr,sizeof(inputStr),stdin);  // INPUT //
+    int j = 0;
 
-                    // Formatting and verifying input string // 
-    //----------------------------------------------------------------------//
-    
-    inputStr[strlen(inputStr)-1]='\0';
-    for(int i=0;i<strlen(inputStr);i++){    
-        if(inputStr[i]==' '){
+    fgets(inputStr, sizeof(inputStr), stdin);  // INPUT //
+
+                    // Formatting and verifying input string 
+    //----------------------------------------------------------------------
+    inputStr[strlen(inputStr) - 1] = '\0';
+    for (int i = 0; i < strlen(inputStr); i++) {
+        if (inputStr[i] == ' ') {
             continue;
         }
-        else if(!isdigit(inputStr[i]) && inputStr[i]!='+' && inputStr[i]!='-' && inputStr[i]!='/' && inputStr[i]!='*' && inputStr[i]!='.' ){
+        else if (!isdigit(inputStr[i]) && inputStr[i] != '+' && inputStr[i] != '-' && inputStr[i] != '/' && inputStr[i] != '*') {
             printf("Invalid String");
             return 0;
         }
-        else{
-            calStr[j]=inputStr[i];
+        else {
+            calStr[j] = inputStr[i];
             j++;
         }
     }
-    calStr[j]='\0';
-    // calStr[j]='\0';
-    // calStr[strlen(calStr)-2]='\0';
-    //Basic calc operation logic//
-    //----------------------------------------------------------------------//
-    
-    char strNum1[100];
-    char op;
-    
-    int i=0;
-    int idx_num1=0;
-    
-    //First num
-    while(i<strlen(calStr) && isdigit(calStr[i])){
-        strNum1[idx_num1]=calStr[i];
-        i++;
-        idx_num1++;
+    calStr[j] = '\0';
+
+                    // Handling * and / operators
+    //----------------------------------------------------------------------
+    int i = 0;
+    char tempStr[1000] = "";
+    while (i < strlen(calStr)) {
+        char num1Str[100] = "";
+        char num2Str[100] = "";
+        int idx1 = 0, idx2 = 0;
+        int num1, num2;
+        char op;
+
+        // Extract first number
+        while (i < strlen(calStr) && isdigit(calStr[i])) {
+            num1Str[idx1++] = calStr[i++];
+        }
+        num1Str[idx1] = '\0';
+        num1 = atoi(num1Str);
+
+        if (i >= strlen(calStr)) {
+            char temp[50];
+            sprintf(temp, "%d", num1);
+            strcat(tempStr, temp);
+            break;
+        }
+
+        op = calStr[i++];
+
+        // process all upcoming * and /
+        while (op == '*' || op == '/') {
+            // Extract second number
+            idx2 = 0;
+            while (i < strlen(calStr) && isdigit(calStr[i])) {
+                num2Str[idx2++] = calStr[i++];
+            }
+            num2Str[idx2] = '\0';
+            num2 = atoi(num2Str);
+
+            if (op == '*') {
+                num1 *= num2;
+            }
+            else if (op == '/') {
+                if (num2 == 0) {
+                    printf("Division by Zero occured");
+                    return 0;
+                }
+                num1 /= num2;
+            }
+
+            // Checking if there is another * or /
+            if (i < strlen(calStr)) {
+                op = calStr[i++];
+            } else {
+                break;
+            }
+        }
+
+        // Storing the result
+        char temp[50];
+        sprintf(temp, "%d", num1);
+        strcat(tempStr, temp);
+
+        if (op == '+' || op == '-') {
+            int len = strlen(tempStr);
+            tempStr[len] = op;
+            tempStr[len + 1] = '\0';
+        }
     }
-    strNum1[idx_num1]='\0';
-    
-    num1=atof(strNum1);
-    while(i<strlen(calStr)){
-        op=calStr[i];
-        i++;
-        // Second num
-        char strNum2[100];
-        int idx_num2=0;
-        while(i<strlen(calStr) && isdigit(calStr[i])){
-            strNum2[idx_num2]=calStr[i];
-            idx_num2++;
+
+                        // Handling + and - operators
+    //----------------------------------------------------------------------
+    int finalResult = 0;
+    i = 0;
+    int sign = 1;
+    while (i < strlen(tempStr)) {
+        char numStr[100] = "";
+        int idx = 0;
+
+        if (tempStr[i] == '+') {
+            sign = 1;
             i++;
         }
-        strNum2[idx_num2]='\0';
-        
-        //Arithmetic operation
-        num2=atof(strNum2);
-        switch(op){
-            case '+':
-            num1+=num2;
-            break;
-            case '-':
-            num1-=num2;
-            break;
-            case '*':
-            num1*=num2;
-            break;
-            case '/':
-            num1/=num2;
-            break;
+        else if (tempStr[i] == '-') {
+            sign = -1;
+            i++;
         }
-    }
-    printf("%lf",num1);
 
+        while (i < strlen(tempStr) && isdigit(tempStr[i])) {
+            numStr[idx++] = tempStr[i++];
+        }
+        numStr[idx] = '\0';
+
+        finalResult += sign * atoi(numStr);
+    }
+
+    printf("%d", finalResult);
+    return 0;
 }
