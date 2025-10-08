a31e5def29f6df092dc836c945e915434fd1f936
Nitin1112 <nitinboopathy11@gmail.com>
2025-10-08 17:56:43 +0530

    updated user structure and added id

--- CRUD.c
+++ CRUD.c
@@ -6,29 +6,39 @@
 
 typedef struct
 {
+    int id;
     char name[30];
-    char email[50];
-    char designation[20];
-    int salary;
+    int age;
 } User;
+
+int getNextId()
+{
+    FILE *f = fopen("user.txt", "r");
+    if (!f)
+        return 1;
+    char line[LINE_LENGTH];
+    int lastId = 0, id;
+    while (fgets(line, sizeof(line), f))
+    {
+        if (strstr(line, "\"id\":") != NULL)
+        {
+            sscanf(line, " \"id\": %d", &id);
+            if (id > lastId)
+                lastId = id;
+        }
+    }
+    fclose(f);
+    return lastId + 1;
+}
 
 void getData(User *user)
 {
     printf("Name: ");
     fgets(user->name, sizeof(user->name), stdin);
     user->name[strcspn(user->name, "\n")] = 0;
-
-    printf("Email: ");
-    fgets(user->email, sizeof(user->email), stdin);
-    user->email[strcspn(user->email, "\n")] = 0;
-
-    printf("Designation: ");
-    fgets(user->designation, sizeof(user->designation), stdin);
-    user->designation[strcspn(user->designation, "\n")] = 0;
-
-    printf("Salary: ");
-    scanf("%d", &user->salary);
-    getchar(); 
+    printf("Age: ");
+    scanf("%d", &user->age);
+    getchar();
 }
 
 void createUser()
@@ -42,17 +52,17 @@ void createUser()
 
     User user;
     printf("\nENTER USER DATA\n\n");
+    user.id = getNextId();
     getData(&user);
 
     fprintf(f, "{\n");
+    fprintf(f, "\"id\": %d,\n", user.id);
     fprintf(f, "\"name\": \"%s\",\n", user.name);
-    fprintf(f, "\"email\": \"%s\",\n", user.email);
-    fprintf(f, "\"designation\": \"%s\",\n", user.designation);
-    fprintf(f, "\"salary\": %d\n", user.salary);
+    fprintf(f, "\"age\": %d\n", user.age);
     fprintf(f, "}\n\n");
 
     fclose(f);
-    printf("User created successfully.\n");
+    printf("User created successfully. (ID: %d)\n", user.id);
 }
 
 void readUser()
@@ -74,37 +84,35 @@ void readUser()
     fclose(f);
 }
 
-void searchAndUpdate(char *searchName, FILE *temp, FILE *f, int *found)
+void searchAndUpdate(int searchId, FILE *temp, FILE *f, int *found)
 {
     char line[LINE_LENGTH];
     User user;
-    char buffer[5000] = "";
 
     while (fgets(line, sizeof(line), f))
     {
-        if (strstr(line, "\"name\":") != NULL)
+        if (strstr(line, "\"id\":") != NULL)
         {
-            char currentName[30];
-            sscanf(line, " \"name\": \"%[^\"]", currentName);
+            int currentId;
+            sscanf(line, " \"id\": %d", &currentId);
 
-            if (strcmp(currentName, searchName) == 0)
+            if (currentId == searchId)
             {
                 *found = 1;
-
                 while (fgets(line, sizeof(line), f))
                 {
                     if (strchr(line, '}'))
                         break;
                 }
 
-                printf("\nEnter new data for %s\n", searchName);
+                printf("\nEnter new data for ID %d\n", searchId);
                 getData(&user);
+                user.id = searchId;
 
                 fprintf(temp, "{\n");
+                fprintf(temp, "\"id\": %d,\n", user.id);
                 fprintf(temp, "\"name\": \"%s\",\n", user.name);
-                fprintf(temp, "\"email\": \"%s\",\n", user.email);
-                fprintf(temp, "\"designation\": \"%s\",\n", user.designation);
-                fprintf(temp, "\"salary\": %d\n", user.salary);
+                fprintf(temp, "\"age\": %d\n", user.age);
                 fprintf(temp, "}\n\n");
             }
             else
@@ -134,13 +142,13 @@ void updateUser()
         return;
     }
 
-    char searchName[30];
-    printf("Enter name of user to update: ");
-    fgets(searchName, sizeof(searchName), stdin);
-    searchName[strcspn(searchName, "\n")] = 0;
+    int searchId;
+    printf("Enter ID of user to update: ");
+    scanf("%d", &searchId);
+    getchar();
 
     int found = 0;
-    searchAndUpdate(searchName, temp, f, &found);
+    searchAndUpdate(searchId, temp, f, &found);
 
     fclose(f);
     fclose(temp);
@@ -169,10 +177,10 @@ void deleteUser()
         return;
     }
 
-    char searchName[30];
-    printf("Enter name of user to delete: ");
-    fgets(searchName, sizeof(searchName), stdin);
-    searchName[strcspn(searchName, "\n")] = 0;
+    int searchId;
+    printf("Enter ID of user to delete: ");
+    scanf("%d", &searchId);
+    getchar();
 
     char line[LINE_LENGTH];
     int found = 0;
@@ -180,12 +188,12 @@ void deleteUser()
 
     while (fgets(line, sizeof(line), f))
     {
-        if (strstr(line, "\"name\":") != NULL)
+        if (strstr(line, "\"id\":") != NULL)
         {
-            char currentName[30];
-            sscanf(line, " \"name\": \"%[^\"]", currentName);
+            int currentId;
+            sscanf(line, " \"id\": %d", &currentId);
 
-            if (strcmp(currentName, searchName) == 0)
+            if (currentId == searchId)
             {
                 found = 1;
                 skipBlock = 1;
