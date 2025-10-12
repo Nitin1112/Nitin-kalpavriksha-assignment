869b455c07bd400e3d86e67e0c588f1ffeed7222
Nitin1112 <nitinboopathy11@gmail.com>
2025-10-12 15:42:42 +0530

    Resolved all comments

--- student-analyzer.c
+++ student-analyzer.c
@@ -1,127 +1,101 @@
 #include <stdio.h>
+#include <string.h>
+
 #define NAME_LENGTH 30
+#define TOTAL_SUBJECTS 3
+
+#define GRADE_A_LIMIT 85
+#define GRADE_B_LIMIT 70
+#define GRADE_C_LIMIT 50
+#define GRADE_D_LIMIT 35
 
 typedef struct
 {
-    int roll;
+    int rollNumber;
     char name[NAME_LENGTH];
-    int mark1;
-    int mark2;
-    int mark3;
-    int total;
-    double average;
-    char grade;
+    int science;
+    int math;
+    int social;
+    int totalMarks;
 } Student;
 
-void analyzePerformance(char grade);
-double averageMark(int* total);
-char getGrade(double average);
-int menu();
-void output(Student *s);
-void printDetails(Student *students, int n);
-void printStar(int n);
-void recursiveRollList(Student *students, int n, int i);
-void studentPerformanceAnalayzer();
-int totalMarks(int m1, int m2, int m3);
-
-int menu()
+typedef enum performance
 {
-    int option;
-    while (1)
-    {
-        int option;
-        printf("\n====STUDENT PERFORMANCE ANALYZER====\n\n");
-        printf("1. Enter Student-Performance-Analayzer\n");
-        printf("2. Exit\n");
-        printf("Enter an option: ");
-        scanf("%d", &option);
-        switch (option)
-        {
-        case 1:
-            studentPerformanceAnalayzer();
-            break;
-        case 2:
-            printf("Exiting...");
-            return 1;
-            break;
-        default:
-            printf("Invalid option");
-        }
-    }
-}
+    A = 5,
+    B = 4,
+    C = 3,
+    D = 2,
+} performance;
 
-int totalMarks(int m1, int m2, int m3)
+double calculateAverageMark(int totalMarks)
 {
-    return m1 + m2 + m3;
-}
-
-double averageMark(int *total)
-{
-    return (double)(*total) / 3.0;
-}
-
-void printStar(int n)
-{
-    for (int i = 0; i < n; i++)
-        printf("*");
-}
-
-void analyzePerformance(char grade)
-{
-    switch (grade)
-    {
-    case 'A':
-        printStar(5);
-        break;
-    case 'B':
-        printStar(4);
-        break;
-    case 'C':
-        printStar(3);
-        break;
-    case 'D':
-        printStar(2);
-        break;
-    }
-}
-
-void output(Student *s)
-{
-    printf("Roll-No: %d\n", s->roll);
-    printf("Name: %s\n", s->name);
-    printf("Total: %d\n", s->total);
-    printf("Average: %f\n", s->average);
-    printf("Grade: %c\n", s->grade);
-    if (s->grade != 'F')
-    {
-        printf("Performance: ");
-        analyzePerformance(s->grade);
-    }
-    printf("\n\n");
+    return (double)(totalMarks) / (TOTAL_SUBJECTS * 1.0);
 }
 
 char getGrade(double average)
 {
-    if (average >= 85)
+    if (average >= GRADE_A_LIMIT)
     {
         return 'A';
     }
-    else if (average >= 70)
+    else if (average >= GRADE_B_LIMIT)
     {
         return 'B';
     }
-    if (average >= 50)
+    else if (average >= GRADE_C_LIMIT)
     {
         return 'C';
     }
-    if (average >= 35)
+    else if (average >= GRADE_D_LIMIT)
     {
         return 'D';
     }
-    if (average < 35)
+    else
     {
         return 'F';
     }
+}
+
+void analyzePerformance(performance p)
+{
+    for (int i = 0; i < p; i++)
+        printf("*");
+}
+
+int calculateTotalMarks(Student *student)
+{
+    return student->science + student->math + student->social;
+}
+
+void output(Student *s)
+{
+    printf("Roll: %d\n", s->rollNumber);
+    printf("Name: %s\n", s->name);
+    printf("Total: %d\n", s->totalMarks);
+    double average = calculateAverageMark(s->totalMarks);
+    printf("Average: %f\n", average);
+    printf("Grade: %c\n", getGrade(average));
+    if (getGrade(average) != 'F')
+    {
+        printf("Performance:");
+        switch (getGrade(average))
+        {
+            case 'A':
+                analyzePerformance(A);
+                break;
+            case 'B':
+                analyzePerformance(B);
+                break;
+            case 'C':
+                analyzePerformance(C);
+                break;
+            case 'D':
+                analyzePerformance(D);
+                break;
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 void printDetails(Student *students, int n)
@@ -132,47 +106,94 @@ void printDetails(Student *students, int n)
     }
 }
 
-void recursiveRollList(Student *students, int n, int i)
+void printAllRollNumbers(Student *students, int n, int i)
 {
-    if (i < n)
+    if (i >= n)
     {
-        printf("%d ", students[i++].roll);
-        recursiveRollList(students, n, i);
+        return;
+    }
+    printf("%d ", students[i++].rollNumber);
+    printAllRollNumbers(students, n, i);
+}
+
+int checkMarkRange(int m)
+{
+    if (m < 0 || m > 100)
+        return 1;
+    return 0;
+}
+
+int handleInput(Student *students, Student *s, int count)
+{
+    for (int index = 0; index < count; index++)
+    {
+        if (students[index].rollNumber == s->rollNumber)
+        {
+            printf("You entered the same Roll Number again !");
+            return 0;
+        }
+    }
+
+    if (checkMarkRange(s->math) || checkMarkRange(s->science) || checkMarkRange(s->social))
+    {
+        printf("Enter valid mark range !");
+        return 0;
+    }
+    return 1;
+}
+
+void sortByRollNumber(Student *students, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (students[j].rollNumber > students[j + 1].rollNumber)
+            {
+                Student temp = students[j];
+                students[j] = students[j + 1];
+                students[j + 1] = temp;
+            }
+        }
     }
 }
 
-void studentPerformanceAnalayzer()
+void studentPerformanceAnalyzer()
 {
-    int noOfStudents;
-    int i = 0;
+    int numberOfStudents;
+    int index = 0;
 
     printf("Enter number of students:");
-    scanf("%d", &noOfStudents);
+    scanf("%d", &numberOfStudents);
 
-    int n = noOfStudents;
+    int n = numberOfStudents;
     Student students[n];
 
-    printf("Enter Student details\n");
-    printf("(Roll-no Name Mark1 Mark2 Mark3):\n");
-
-    while (i < n)
+    while (index < n)
     {
         Student s;
-        scanf("%d %s %d %d %d", &s.roll, s.name, &s.mark1, &s.mark2, &s.mark3);
-
-        s.total = totalMarks(s.mark1, s.mark2, s.mark3);
-        s.average = averageMark(&s.total);
-        s.grade = getGrade(s.average);
-
-        students[i++] = s;
+        scanf("%d %s %d %d %d", &s.rollNumber, s.name, &s.science, &s.math, &s.social);
+        if (!handleInput(students, &s, index))
+        {
+            return;
+        }
+        s.totalMarks = calculateTotalMarks(&s);
+        students[index++] = s;
     }
 
+    sortByRollNumber(students, n);
+
     printDetails(students, n);
-    printf("List of Roll Numbers: ");
-    recursiveRollList(students, n, 0);
+    printf("List of Roll Numbers(via recursion):");
+    printAllRollNumbers(students, n, 0);
+    printf("\n");
 }
 
 int main()
 {
-    return menu();
+    while (1)
+    {
+        studentPerformanceAnalyzer();
+    }
+    return 0;
 }
